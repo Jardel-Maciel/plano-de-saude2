354e868b056bba226ac8f2562b7bf21205fee328
jardel <jardel.maciel22@gmail.com>
2025-03-28 09:07:41 -0300

    Update app.js

--- plano-de-saude/app.js
+++ plano-de-saude/app.js
@@ -1,7 +1,33 @@
-var idade = document.getElementById('idade');
-var idadeDoCliente = parseInt(idade);
-var nome = document.getElementById('nome');
 
-function calcularPlano(){
-    if(idade = 0 && idade < 19)//formular calculo da função
+
+function calcular(){
+    var idade = document.getElementById('idade');
+    var idadeDoCliente = Number(idade.value);
+    var valor = document.getElementById('valorBase');
+    var valorBase = Number(valor.value);
+    var res = document.getElementById('resposta');
+    if(idadeDoCliente => 0 && idadeDoCliente < 19){
+        var valorMensal = valorBase * 100/100;
+        res.innerHTML = ("O valor do plano é de R$" + valorMensal + " por mês") 
+    }
+    if(idadeDoCliente >= 19 && idadeDoCliente < 29){
+        var valorMensal = valorBase * 150/100;
+        res.innerHTML = ("O valor do plano é de R$" + valorMensal + " por mês")
+    }
+    if(idadeDoCliente >= 29 && idadeDoCliente < 39){
+        var valorMensal = valorBase * 225/100;
+        res.innerHTML = ("O valor do plano é de R$" + valorMensal + " por mês")
+    }
+    if(idadeDoCliente >= 39 && idadeDoCliente < 49){
+        var valorMensal = valorBase * 240/100;
+        res.innerHTML = ("O valor do plano é de R$" + valorMensal + " por mês")
+    }
+    if(idadeDoCliente >= 49 && idadeDoCliente < 59){
+        var valorMensal = valorBase * 350/100;
+        res.innerHTML = ("O valor do plano é de R$" + valorMensal + " por mês")
+    }
+    if(idadeDoCliente > 60){
+        var valorMensal = valorBase * 600/100;
+        res.innerHTML = ("O valor do plano é de R$" + valorMensal + " por mês")
+    }
 }
