e7944bfeba5c843ea6de859aa69607f1c593562b
jardel <jardel.maciel22@gmail.com>
2025-03-27 21:03:20 -0300

    creat app.js

---
+++ plano-de-saude/app.js
@@ -0,0 +1,7 @@
+var idade = document.getElementById('idade');
+var idadeDoCliente = parseInt(idade);
+var nome = document.getElementById('nome');
+
+function calcularPlano(){
+    if(idade = 0 && idade < 19)
+}
