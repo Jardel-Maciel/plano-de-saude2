8ced88a8b4739675c91754e1cc6c32fa5fa5b61d
jardel <jardel.maciel22@gmail.com>
2025-03-27 21:04:25 -0300

    Update app.js

--- plano-de-saude/app.js
+++ plano-de-saude/app.js
@@ -3,5 +3,5 @@ var idadeDoCliente = parseInt(idade);
 var nome = document.getElementById('nome');
 
 function calcularPlano(){
-    if(idade = 0 && idade < 19)
+    if(idade = 0 && idade < 19)//formular calculo da função
 }
